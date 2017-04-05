694fabfc21ee0bf8fbcf61f79a9f90680cc3d1c9
Tim Heisler <timheisler@gmail.com>
2017-04-04 23:04:28 -0400

    Test commit from my laptop

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,56 +1,57 @@
 /// <reference path="../../node_modules/lz-string/typings/lz-string.d.ts" />
-import { Injectable} from '@angular/core';
-import {BehaviorSubject} from 'rxjs/BehaviorSubject';
-import {Hive} from 'app/classes/hive.class';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Hive } from 'app/classes/hive.class';
 import * as Bee from './classes/bee.class';
 
 
 @Injectable()
-export class GameService { 
-    hives:Hive[];   
-    saveTime:number;
-    lastTime:number;
-    stepTimeMs:number
+export class GameService {
+    hives: Hive[];
+    saveTime: number;
+    lastTime: number;
+    stepTimeMs: number
     private _elapsedMs = new BehaviorSubject<number>(0);
     private _running = new BehaviorSubject<boolean>(true);
     gameLoopEvent$ = this._elapsedMs.asObservable();
     stateChangeEvent$ = this._running.asObservable();
-    constructor() {        
+    constructor() {
         this.saveTime = Date.now();
         //this.lastTime = this.saveTime - Date.now();
-        this.stepTimeMs = 1000;        
+        this.stepTimeMs = 1000;
         this.initGame();
     }
 
     initGame() {
         this.hives = [];
         var bees = [];
-        bees.push(new Bee.Queen({id:"1-H2"}))
-        this.hives.push(new Hive({id: 1, nextId: 1, bees: bees}));
-        var json = JSON.stringify(this.hives[0]);  
+        bees.push(new Bee.Queen({ id: "1-H1" }))
+        this.hives.push(new Hive({ id: 1, nextId: 1, bees: bees }));
+        var json = JSON.stringify(this.hives[0]);
         var hive2 = JSON.parse(json);
-        hive2.id = 2;  
-        this.hives.push(new Hive(JSON.parse(json)));        
-        this.gameLoop(0); 
+        hive2.id = 2;
+        hive2.bees[0].id = "1-H2";
+        this.hives.push(new Hive(hive2));
+        this.gameLoop(0);
     }
 
     gameLoop(step) {
         this.saveTime = Date.now();
-        if(this.lastTime == null) this.lastTime = this.saveTime - Date.now();
+        if (this.lastTime == null) this.lastTime = this.saveTime - Date.now();
         var steps = 0;
-        while(step-this.lastTime>=(this.stepTimeMs * (steps+1))) {
+        while (step - this.lastTime >= (this.stepTimeMs * (steps + 1))) {
             steps++;
         }
         this.lastTime += (this.stepTimeMs * steps);
-        if(this._running.value && steps>0)  {      
+        if (this._running.value && steps > 0) {
             this._elapsedMs.next(this.stepTimeMs * steps);
-            this.hives[0].bees[0].msSinceWork += this.stepTimeMs * steps;            
+            this.hives[0].bees[0].msSinceWork += this.stepTimeMs * steps;
         }
         window.requestAnimationFrame(this.gameLoop.bind(this));
-        
+
     }
 
-    getHives():Hive[] {
+    getHives(): Hive[] {
         return this.hives;
     }
 
